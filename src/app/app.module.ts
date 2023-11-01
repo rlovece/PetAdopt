c7fc80d6eb830f4f7f78bc590a709c2fc365e9c9
rlovece <ruth.lovece@gmail.com>
2023-11-01 12:12:13 -0300

    Se agrega nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shares/footer/footer.component';
+import { NavComponent } from './shares/nav/nav.component';
+import { HeaderComponent } from './shares/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
