3b5218b73080d7ac77a0c80426e4bb2ec2e8fd40
rlovece <ruth.lovece@gmail.com>
2023-11-03 21:15:28 -0300

    lista.mascotas

--- src/app/modules/mascotas/services/mascotas.service.ts
+++ src/app/modules/mascotas/services/mascotas.service.ts
@@ -1,26 +1,53 @@
-import { Injectable, OnInit } from '@angular/core';
-import { Observable, lastValueFrom, toArray } from 'rxjs';
-import { ApiMascotasService } from 'src/app/core/api-mascotas.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Mascota } from 'src/app/core/models/mascota.models';
+import { environments } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MascotasService implements OnInit {
 
-  mascotas: Mascota[] = [];
+export class MascotasService {
 
-  constructor(
-    private apiMascotasService : ApiMascotasService
-  ) { }
+  baseURL: string = environments.baseURL;
 
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.getAll();
+  getAll(): Observable<Mascota[]>{
+    return this.http.get<Mascota[]>(`${this.baseURL}/mascotas`);
   }
 
-  getAll() : Observable<Mascota[]>{
-    return this.apiMascotasService.getAll();
+  getById(id: number): Observable<Mascota>{
+    return this.http.get<Mascota>(`${this.baseURL}/mascotas/${id}`);
+  }
+
+  getByTipo(tipo: string): Observable<Mascota[]>{
+    return this.http.get<Mascota[]>(`${this.baseURL}/mascotas?tipo=${tipo}`);
+  }
+
+  getByEstado(estado: string): Observable<Mascota[]>{
+    return this.http.get<Mascota[]>(`${this.baseURL}/mascotas?estado=${estado}`);
+  }
+
+  getByTamanio(tamanio: string): Observable<Mascota[]>{
+    return this.http.get<Mascota[]>(`${this.baseURL}/mascotas?tamanio=${tamanio}`);
+  }
+
+  getByEdad(edad: string): Observable<Mascota[]>{
+    return this.http.get<Mascota[]>(`${this.baseURL}/mascotas?edad=${edad}`);
+  }
+
+  create(mascota: Mascota){
+    return this.http.post<Mascota>(this.baseURL, mascota);
+  }
+
+  update(id: number, mascota: Partial<Mascota>){
+    return this.http.put<Mascota>(`${this.baseURL}/${id}`, mascota);
+  }
+
+  delete(id: number){
+    return this.http.delete<boolean>(`${this.baseURL}/${id}`);
   }
 
 }
