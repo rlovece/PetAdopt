d77661bef50711c4ff3630c892d456e6f6f8e14b
rlovece <ruth.lovece@gmail.com>
2023-11-10 15:19:27 -0300

    erutamiento

--- src/app/modules/administracion/administracion-routing.module.ts
+++ src/app/modules/administracion/administracion-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PanelAdministracionComponent } from './pages/panel-administracion/panel-administracion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PanelAdministracionComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
