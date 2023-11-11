be5f98ffb0c29419918bd1371fb35efe772b2b11
rlovece <ruth.lovece@gmail.com>
2023-11-11 15:47:48 -0300

    Se agregan filtros para admin en mascotas y edit

---
+++ src/app/modules/administracion/gestion-mascotas/gestion-mascotas.component.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { Mascota } from 'src/app/core/models/Models/mascota';
+import { MascotasService } from 'src/app/core/services/mascotas.service';
+
+@Component({
+  selector: 'app-gestion-mascotas',
+  templateUrl: './gestion-mascotas.component.html',
+  styleUrls: ['./gestion-mascotas.component.css']
+})
+export class GestionMascotasComponent {
+
+  listaCompletaMascotas: Mascota[] = [];
+  listaFiltradaMascotas: Mascota[] = [];
+  opcionSeleccionada: string = 'bienvenida';
+  mostrarPanelAddMascota: boolean = false;
+  mostrarPanelEditMascota: boolean = false;
+  mascotaSeleccionadaParaEdicion: Mascota | null = null;
+
+  constructor(
+    private mascotasService: MascotasService,
+  ){}
+
+  actualizarOpcion(opcion: string) {
+    this.opcionSeleccionada = opcion;
+  }
+
+  ngOnInit(){
+    this.getAll();
+  }
+
+  getAll(){
+    this.mascotasService.getAll()
+    .subscribe(
+      {
+        next: data => {
+          this.listaCompletaMascotas=data;
+          this.listaFiltradaMascotas = this.listaCompletaMascotas.slice();
+        },
+        error: e => console.log(e)
+      }
+    )
+  }
+
+  verPanelAddMascota() {
+    this.mostrarPanelAddMascota = !this.mostrarPanelAddMascota;
+  }
+
+  onAgregarMascota(newMascota : Mascota){
+    this.mostrarPanelAddMascota = false;
+    this.listaCompletaMascotas.push(newMascota);
+    this.listaFiltradaMascotas.push(newMascota);
+  }
+
+  verPerros(){
+    this.listaFiltradaMascotas =
+    this.listaFiltradaMascotas.filter(m => m.tipo == 'Perro');
+  }
+
+
+  verGatos(){
+    this.listaFiltradaMascotas =
+    this.listaFiltradaMascotas.filter(m => m.tipo == 'Gato');
+  }
+
+  verAdopcion(){
+    this.listaFiltradaMascotas =
+    this.listaFiltradaMascotas.filter(m => m.estado == 'En adopcion');
+  }
+
+  quitarFiltros(){
+    this.listaFiltradaMascotas = this.listaCompletaMascotas.slice();
+  }
+
+  verPanelEditMascota() {
+    this.mostrarPanelEditMascota = !this.mostrarPanelEditMascota;
+  }
+
+  editMascota(mascota: Mascota){
+    console.log("Mascota recibida ", mascota);
+    this.mascotaSeleccionadaParaEdicion = mascota;
+  }
+
+  deleteMascota(mascota: Mascota){
+
+  }
+
+}
