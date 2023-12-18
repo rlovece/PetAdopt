70487653cd8782e5bee3fa4d48f0142c70fe415c
rlovece <ruth.lovece@gmail.com>
2023-12-17 22:20:33 -0300

    Se agregan filtros en landing}

--- src/app/modules/landing/components/lista-adopcion/lista-adopcion.component.ts
+++ src/app/modules/landing/components/lista-adopcion/lista-adopcion.component.ts
@@ -8,9 +8,8 @@ import { MascotasService } from 'src/app/core/services/api-mascotas.service';
   styleUrls: ['./lista-adopcion.component.css']
 })
 export class ListaAdopcionComponent {
-
   mascotasEnAdopcion: Mascota[] = [];
-
+  mascotasEnAdopcionFiltrada: Mascota[] = [];
 
   constructor(
     private mascotasService: MascotasService,
@@ -24,10 +23,42 @@ export class ListaAdopcionComponent {
     this.mascotasService.getAll()
     .subscribe(
       {
-        next: data => this.mascotasEnAdopcion=data.filter(m => m.estado == 'En adopcion'),
+        next: data => {
+          this.mascotasEnAdopcion=data.filter(m => m.estado == 'En adopcion');
+          this.mascotasEnAdopcionFiltrada = this.mascotasEnAdopcion.slice();
+        },
         error: e => console.log(e)
       }
     )
+  }
+
+  verPerros(){
+    this.mascotasEnAdopcionFiltrada =
+    this.mascotasEnAdopcionFiltrada.filter(m => m.tipo == 'Perro');
+  }
+
+  verGatos(){
+    this.mascotasEnAdopcionFiltrada =
+    this.mascotasEnAdopcionFiltrada.filter(m => m.tipo == 'Gato');
+  }
+
+  quitarFiltros(){
+    this.mascotasEnAdopcionFiltrada = this.mascotasEnAdopcion.slice();
+  }
+
+  verPequenios() {
+    this.mascotasEnAdopcionFiltrada =
+    this.mascotasEnAdopcionFiltrada.filter(m => m.tamanio == 'Pequeño');
+  }
+
+  verMedianos() {
+    this.mascotasEnAdopcionFiltrada =
+    this.mascotasEnAdopcionFiltrada.filter(m => m.tamanio == 'Mediano');
+  }
+
+  verGrandes() {
+    this.mascotasEnAdopcionFiltrada =
+    this.mascotasEnAdopcionFiltrada.filter(m => m.tamanio == 'Grande');
   }
 
 }
